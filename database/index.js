c6cb93f3d3e1f948e78595bebf3a2ab5246e48b1
irfan1991 <irfanprasetyo91@gmail.com>
2020-11-05 10:37:53 +0700

    conig

--- database/index.js
+++ database/index.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose')
 
 const {dbHost, dbName, dbPort, dbUser, dbPass} = require('../app/config')
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`, 
+// mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`, 
+// {useNewUrlParser:true, useUnifiedTopology : true, useFindAndModify: false}
+// );
+
+mongoose.connect(`mongodb://heroku_9vf716br:rTUc3BwTU7bgK3@@ds127300.mlab.com:27300/heroku_9vf716br`, 
 {useNewUrlParser:true, useUnifiedTopology : true, useFindAndModify: false}
 );
-
 const db = mongoose.connection;
 
 module.exports = db
