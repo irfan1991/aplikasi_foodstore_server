227990c6d12780f6f4ebe72e30bbb87fafd4fefa
irfan1991 <irfanprasetyo91@gmail.com>
2020-11-05 10:44:39 +0700

    conig

--- database/index.js
+++ database/index.js
@@ -6,7 +6,7 @@ const {dbHost, dbName, dbPort, dbUser, dbPass} = require('../app/config')
 // {useNewUrlParser:true, useUnifiedTopology : true, useFindAndModify: false}
 // );
 
-mongoose.connect(`mongodb://heroku_9vf716br:rTUc3BwTU7bgK3@@ds127300.mlab.com:27300/heroku_9vf716br`, 
+mongoose.connect(process.env.MONGODB_URI ||`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`, 
 {useNewUrlParser:true, useUnifiedTopology : true, useFindAndModify: false}
 );
 const db = mongoose.connection;
